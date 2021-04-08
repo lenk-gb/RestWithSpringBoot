4a3399e4609dc277d3c18b7ed62ef383893e28f7
Hugo Anjos <hugodaniel.anjos@gmail.com>
2021-04-08 17:11:10 -0300

    Criação de metodo para o primeiro  Hello World

---
+++ rest-with-spring-boot-udemy/src/main/java/br/com/hugoanjos/restwithspringbootudemy/Greeting.java
@@ -0,0 +1,20 @@
+package br.com.hugoanjos.restwithspringbootudemy;
+
+public class Greeting {
+    private Long id;
+    private String content;
+
+    public Greeting(Long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
